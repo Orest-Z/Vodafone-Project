45f58ab5e3139d9dd03d5c76af73740a343c0616
Orest-Z <orest.zogju@fshnstudent.info>
2026-08-16 15:46:46 +0200

    modified the activation steps components to now use icons number, and also a class for the fade animations that were done in css

--- components/ActivationStep.tsx
+++ components/ActivationStep.tsx
@@ -2,14 +2,18 @@ export const ActivationStep = ({
   number,
   title,
   text,
+  icon,
 }: {
   number: string;
   title: string;
   text: string;
+  icon: React.ReactNode;
 }) => (
-  <div className="step">
-    How to Activate
-    <div className="step-number">{number}</div>
+  <div className="step fade-in-up-stagger">
+    <div className="step-header">
+      <div className="step-icon-wrapper">{icon}</div>
+      <div className="step-number-badge">{number}</div>
+    </div>
     <h3 className="step-title">{title}</h3>
     <p className="step-text">{text}</p>
   </div>
